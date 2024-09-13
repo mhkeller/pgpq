bc4960628d984a7d41d0e06d3e82dac24c993672
mhkeller <code@mhkeller.com>
2024-09-13 16:30:18 -0400

    redo cli to be more like yellow cab test

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,12 @@
 use arrow::record_batch::RecordBatch;
 use bytes::BytesMut;
 use clap::{App, Arg};
-use parquet::arrow::arrow_reader::ParquetRecordBatchReader;
+use parquet::arrow::arrow_reader::{ParquetRecordBatchReaderBuilder};
+use arrow::record_batch::RecordBatchReader;
 use pgpq::ArrowToPostgresBinaryEncoder;
 use std::fs::File;
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
+use arrow::datatypes::{Schema};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("pgpq")
@@ -23,50 +25,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let input = matches.value_of("input").unwrap();
 
-    let record_batches = if input == "-" {
-        read_parquet_from_stdin()?
-    } else {
-        read_parquet_from_file(input)?
-    };
+    let (batches, schema) = read_parquet_from_file(input);
+    let mut encoder = ArrowToPostgresBinaryEncoder::try_new(&schema).unwrap();
+    let mut buf = BytesMut::new();
+    encoder.write_header(&mut buf);
 
-    let mut encoder = ArrowToPostgresBinaryEncoder::try_new(&record_batches[0].schema())?;
-    let mut buffer = BytesMut::new();
+    io::stdout().write_all(&buf)?;
+    buf.clear();
 
-    // Write header
-    encoder.write_header(&mut buffer);
-    io::stdout().write_all(&buffer)?;
-    buffer.clear();
-
-    // Write batches
-    for batch in record_batches {
-        encoder.write_batch(&batch, &mut buffer)?;
-        io::stdout().write_all(&buffer)?;
-        buffer.clear();
+    for batch in batches {
+        encoder.write_batch(&batch, &mut buf).unwrap();
+        io::stdout().write_all(&buf)?;
+        buf.clear();
     }
+    encoder.write_footer(&mut buf).unwrap();
+    io::stdout().write_all(&buf)?;
+    buf.clear();
 
     Ok(())
 }
 
-fn read_parquet_from_stdin() -> Result<Vec<RecordBatch>, Box<dyn std::error::Error>> {
-    let mut buffer = Vec::new();
-    io::stdin().read_to_end(&mut buffer)?;
-    // Implement Parquet reading from buffer
-    // This part needs to be implemented using the parquet crate
-    unimplemented!()
+fn read_parquet_from_file(input: &str) -> (Vec<RecordBatch>, Schema) {
+    // let mut file = download_dataset();
+    let file = File::open(input).expect("failed to open file");
+
+    let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
+    let reader = builder.build().unwrap();
+    let schema = Schema::new(reader.schema().fields().clone());
+    let data: Vec<RecordBatch> = reader.map(|v| v.unwrap()).collect();
+    (data, schema)
 }
 
-fn read_parquet_from_file(path: &str) -> Result<Vec<RecordBatch>, Box<dyn std::error::Error>> {
-    // Open the file
-    let file = File::open(path)?;
-
-    // Create a ParquetRecordBatchReader directly from the file
-    let record_batch_reader = ParquetRecordBatchReader::try_new(file, 8192)?;
-
-    // Read all record batches
-    let mut record_batches = Vec::new();
-    for batch in record_batch_reader {
-        record_batches.push(batch?);
-    }
-
-    Ok(record_batches)
-}
